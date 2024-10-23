c6036a1b0601d8a1a93ba3d4dc75aa29782a62ec
NunesAzevedo <pnazevedo1@gmail.com>
2024-10-23 19:28:58 -0300

    Inclui o arquivo 'ex5.c' do trabalho 01, e criei sua respectiva header Também adicionei as structc 'DADO' e 'CABECALHO' na main.h

--- include/main.h
+++ include/main.h
@@ -52,4 +52,29 @@ typedef struct{
     int P5;
 } no_indice;
 
+typedef struct {
+    char removido;
+    int encadeamento;
+    int populacao;
+    float tamanho;
+    char unidadeMedida;
+    int velocidade;
+    char nome[50];
+    char especie[50];
+    char habitat[50];
+    char tipo[50];
+    char dieta[50];
+    char alimento[50];
+} DADO;
+
+typedef struct {
+    char status;
+    int topo;
+    int proxRRN;
+    int nroRegRem;
+    int nroPagDisco;
+    int qttCompacta;
+} CABECALHO;
+
+
 #endif
