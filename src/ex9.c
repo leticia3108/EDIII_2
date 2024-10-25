281e7de9f891ab57326a5d6c2f3c90b173e78464
NunesAzevedo <pnazevedo1@gmail.com>
2024-10-25 02:24:07 -0300

    Atualizado pra versao em que funciona 9 dos 10 casos e também foi removidas as funções que não são mais usadas e estavam comentadas

--- src/ex9.c
+++ src/ex9.c
@@ -61,8 +61,6 @@ scanf("%s", nome_2);
 char c_zero = '0';
 char c_um = '1';
 
-int* prox = malloc(sizeof(int));
-
 // Abertura do binário de entrada para leitura e alteração de seu status.
 FILE *binario_reescrita = fopen(nome_1,"rb+");
 fseek(binario_reescrita, 0, SEEK_SET);
@@ -148,15 +146,16 @@ for (i = 0; i < n; i++) {
     fread(&RRNraiz, sizeof(int), 1, binario_indice);
     fread(proxRRN, sizeof(int), 1, binario_indice);
     
-    *(prox) = *(proxRRN)-1;
+    //*(proxRRN)=*(proxRRN)-1;
 
-    inserir(ind, binario_indice, RRNraiz, prox);
+    inserir(ind, binario_indice, RRNraiz, proxRRN);
 
     fseek(binario_reescrita, 1, SEEK_SET);
     fwrite(&dado[i].encadeamento, sizeof(int), 1, binario_reescrita);
 }
+    int prox = *(proxRRN)-1;
 
-    ajustaCabecalho(binario_indice,'1',RRNraiz, proxRRN);
+    ajustaCabecalho(binario_indice,'1',RRNraiz, &prox);
 
     // Atualiza o status do arquivo de dados
     fseek(binario_reescrita, 0, SEEK_SET);
