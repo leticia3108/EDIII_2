8754eaa27815ce19a46fe94aa50298b85c218ba7
NunesAzevedo <pnazevedo1@gmail.com>
2024-10-25 02:39:00 -0300

    Correção da funcionalidade 9. Todos os casos funcionando no runcodes!

--- src/ex9.c
+++ src/ex9.c
@@ -61,6 +61,8 @@ scanf("%s", nome_2);
 char c_zero = '0';
 char c_um = '1';
 
+int* prox = malloc(sizeof(int));
+
 // Abertura do binário de entrada para leitura e alteração de seu status.
 FILE *binario_reescrita = fopen(nome_1,"rb+");
 fseek(binario_reescrita, 0, SEEK_SET);
@@ -144,18 +146,18 @@ for (i = 0; i < n; i++) {
 
     fseek(binario_indice, 1, SEEK_SET);
     fread(&RRNraiz, sizeof(int), 1, binario_indice);
-    fread(proxRRN, sizeof(int), 1, binario_indice);
+    fread(prox, sizeof(int), 1, binario_indice);
     
-    //*(proxRRN)=*(proxRRN)-1;
+    *(proxRRN) = *(prox) - 1;
 
     inserir(ind, binario_indice, RRNraiz, proxRRN);
 
     fseek(binario_reescrita, 1, SEEK_SET);
     fwrite(&dado[i].encadeamento, sizeof(int), 1, binario_reescrita);
+    ajustaCabecalho(binario_indice,'1',RRNraiz, proxRRN);
 }
-    int prox = *(proxRRN)-1;
 
-    ajustaCabecalho(binario_indice,'1',RRNraiz, &prox);
+
 
     // Atualiza o status do arquivo de dados
     fseek(binario_reescrita, 0, SEEK_SET);
@@ -171,6 +173,7 @@ free(pop);
 free(vel);
 free(tam);
 free(unidade);
+free(prox);
 fclose(binario_indice);
 fclose(binario_reescrita);
 return;
